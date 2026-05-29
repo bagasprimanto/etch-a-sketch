dde32bc50efda96b1cb650a384718b161c20c321
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2026-05-29 11:19:21 +0700

    Added random coloring mode

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const setNumSquaresBtn = document.querySelector("h2 button");
+const setNumSquaresBtn = document.querySelector("h2 button.set-num-boxes");
+const setColorModeBtn = document.querySelector("h2 button.set-color-mode");
 const container = document.querySelector(".container");
 const containerWidth = container.offsetWidth;
 const containerHeight = container.offsetHeight;
@@ -32,15 +33,50 @@ function createGrid(numSquares) {
             const newDiv = document.createElement("div");
             newDiv.style = `width: ${squareWidth}px;`;
             newDiv.addEventListener("mouseenter", () => {
-                newDiv.classList.add("hovered");
+                newDiv.style.backgroundColor += getShadeColor();
             })
             container.appendChild(newDiv);
         }
     }
 }
 
+function toggleColorMode() {
+    let colorMode = setColorModeBtn.textContent;
+
+    if (colorMode === "Single color") {
+        colorMode = "Random color";
+    } else {
+        colorMode = "Single color";
+    }
+
+    setColorModeBtn.textContent = colorMode;
+}
+
+function getRandomColor() {
+    const colorLetters = "0123456789ABCDEF";
+
+    let colorString = "#";
+
+    for (let i = 0; i < 6; i++) {
+        colorString += colorLetters.at(Math.floor(Math.random() * 16));
+    }
+
+    return colorString;
+}
+
+function getShadeColor() {
+    const colorMode = setColorModeBtn.textContent;
+
+    if (colorMode === "Single color") {
+        return "blue";
+    } else {
+        return getRandomColor();
+    }
+}
+
 setNumSquaresBtn.addEventListener("click", () => {
     createGrid(getNumSquares());
 });
+setColorModeBtn.addEventListener("click", toggleColorMode);
 
 createGrid(DEFAULT_NUM_SQUARES);
