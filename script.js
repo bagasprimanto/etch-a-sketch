fe94dd0d76baecab82ad6e213ac870c9fcecb2f3
bagasprimanto <39642138+bagasprimanto@users.noreply.github.com>
2026-05-29 10:29:18 +0700

    Displayed the 16x16 grid

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const container = document.querySelector(".container");
+const containerWidth = container.offsetWidth;
+const containerHeight = container.offsetHeight;
 
 function getNumBoxes() {
-    let num = prompt("Input the number of boxes");
+    let num = null;
+
+    while (!num) {
+        num = prompt("Input the number of boxes");
+    }
+
+    return num;
 }
+
+function createGrid() {
+    const numSquares = 16;
+
+    const squareWidth = Math.floor(containerWidth / numSquares) - 1;
+    const squareHeight = Math.floor(containerHeight / numSquares) - 1;
+
+    for (let i = 0; i < numSquares; i++) {
+        for (let j = 0; j < numSquares; j++) {
+            const newDiv = document.createElement("div");
+            newDiv.style = `width: ${squareWidth}px; border: 0.5px solid red; background: white;`;
+            container.appendChild(newDiv);
+        }
+    }
+}
+
+createGrid();
